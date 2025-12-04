eeb1b0a3912470902cf2255f99d827dd79c4a1b1
smnmnssn <simonmansson@live.se>
2025-12-04 09:50:27 +0100

    files moved and import paths changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import UserStatus from "./components/UserStatus";
+import UserStatus from "./UserStatus";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
