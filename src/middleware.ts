6b56a846e7efb89c117053a429aa06465c21c277
smnmnssn <simonmansson@live.se>
2025-12-31 19:08:00 +0100

    -added middleware to reject access to pages that require login user

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/login",
+  },
+});
+
+export const config = {
+  matcher: ["/((?!api|_next|favicon.ico|login|register).*)"],
+};
